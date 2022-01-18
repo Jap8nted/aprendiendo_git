71dd8ecd28391757ad9d781208b76600568538ef
santeaponte <santeaponte@gmail.com>
2022-01-18 16:20:50 -0500

    movimiento bolita

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from time import sleep
 import pygame
 import random
@@ -21,26 +20,3 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
-=======
-import pygame
-
-pygame.init()
-
-screen = pygame.display.set_mode([500, 500])
-
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill((255, 255, 255))
-
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 40)
-
-    pygame.display.flip()
-
-
-pygame.quit()
->>>>>>> c0bc6bf851973193a91ef78d7132005b77f49d51
