8b639b4a502c3f5801b6f871b7c1d23d4aaa0585
santeaponte <santeaponte@gmail.com>
2022-01-18 16:16:15 -0500

    movimiento bolita

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # aprendiendo_git
+
+Esto sirve para aprender git, o no?
+pglo
--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
+from time import sleep
 import pygame
+import random
+
 pygame.init()
 
 screen = pygame.display.set_mode([500,500])
-running = True 
-
+running = True                      
 while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((255,255,255))
-
-    pygame.draw.circle(screen,(0,100,155),(250,250),25)
+    x = random.randint(100,400)
+    y = random.randint(100,400)
+    pygame.draw.circle(screen,(0,100,155),(x,y),25)
+   
 
     pygame.display.flip()
 
